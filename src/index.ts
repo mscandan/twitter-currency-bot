21621190221c1d680176eccd9dfc5dae4f7d5637
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-12-20 20:42:10 +0300

    set timezone to istanbul

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,13 @@ const sendTweet = () => {
   requests
     .getLatestCurrencyRate()
     .then(latestRate => {
+      const now = new Date();
       user.v1.tweet(
-        `${new Date().toLocaleDateString('tr')} ${new Date().toLocaleTimeString(
-          'tr',
-        )} 1 USD = ${latestRate} TRY`,
+        `${now.toLocaleDateString('tr', {
+          timeZone: 'Europe/Istanbul',
+        })} ${now.toLocaleTimeString('tr', {
+          timeZone: 'Europe/Istanbul',
+        })} 1 USD = ${latestRate} TRY`,
       );
     })
     .catch(err => console.error(err));
